0920bab74429949f14655d65793397ea2789509b
Matt Pedersen <mpedersen2054@gmail.com>
2014-11-02 20:24:30 -0600

    make ryu- and hadouken element selectors vars so DOM is only queried once

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
 
 $(document).ready(function () {
+  
+  var ryuStill = $('.ryu-still'),
+      ryuReady = $('.ryu-ready'),
+      ryuThrowing = $('.ryu-throwing'),
+      hadouken = $('.hadouken');
 
   $('.ryu').on('mouseenter', function () {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
+    ryuStill.hide();
+    ryuReady.show();
   })
   .on('mouseleave', function () {
-    $('.ryu-still').show();
-    $('.ryu-ready').hide();
+    ryuStill.show();
+    ryuReady.hide();
+  })
+  .on('mousedown', function () {
+    // play hadouken sound
+    ryuReady.hide();
+    ryuThrowing.show();
+    hadouken.show();
+    // animate hadouken to the right of the screen
+  })
+  .on('mouseup', function () {
+    ryuThrowing.hide();
+    ryuReady.show();
   });
 
 });
