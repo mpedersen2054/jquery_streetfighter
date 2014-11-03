0e05f1eac1e3b6a93dad2b32bd714b49f9a8976e
Matt Pedersen <mpedersen2054@gmail.com>
2014-11-02 20:09:20 -0600

    add jquery mouseenter and mouseleave events to display the associated ryu- div

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
 
+$(document).ready(function () {
 
+  $('.ryu').on('mouseenter', function () {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .on('mouseleave', function () {
+    $('.ryu-still').show();
+    $('.ryu-ready').hide();
+  });
+
+});
